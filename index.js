09441b493ee5ab876f803b6734802d2fc9c3e4b7
Jake Rigby <jakewigby@gmail.com>
2014-03-07 17:56:49 +0000

    server side client for evaluations

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports.build = require('./modules/clidb-builder');
 module.exports.socket = require('./modules/clidb-socket');
 module.exports.client = require('./modules/clidb-direct');
-//module.exports.evaluator = require('./modules/clidb-evaluator');
+module.exports.eval = require('./modules/clidb-eval');
 module.exports.provideContent = require('./modules/content-provider');
